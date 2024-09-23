8b189a21f3c6016ca72e647f4d957e90e4326da8
vdsr <valleria@gmail.com>
2024-09-23 17:01:21 +0200

    codingBlockchainClass

---
+++ blockchain.mjs
@@ -0,0 +1,14 @@
+class Blockchain{
+    constructor(){
+        this.chain = [];
+    }
+
+    getChain(){
+        console.log(this.chain);
+    }
+}
+
+export default Blockchain;
+
+const blockchain = new Blockchain();
+blockchain.getChain();
